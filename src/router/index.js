822951af22a05172fccd974eee40c9a856514d44
Alabo Excel <iamalaboexcel@gmail.com>
2022-08-29 15:59:24 +0100

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import routes from './router'
+
+const isAuthenticated = localStorage.getItem('isAuthenticated');
+const router = createRouter({
+  scrollBehavior(to, from, savedPosition) {
+    // always scroll to top
+    return { top: 0 }
+  },
+  history: createWebHistory(),
+  routes
+})
+
+router.beforeEach(async (to, from) => {
+  if (
+    // make sure the user is authenticated
+    !isAuthenticated &&
+    // ❗️ Avoid an infinite redirect
+    to.name !== 'Login'
+  ) {
+    // redirect the user to the login page
+    return { name: 'Login' }
+  }
+})
+export default router
