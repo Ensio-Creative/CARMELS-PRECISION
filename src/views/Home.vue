d4f5297e2996ed12c043b25a96031fb9e5b21271
alabo-excel <iamalaboexcel@gmail.com>
2022-11-17 01:45:33 -1100

    structure

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,67 +7,7 @@ document.title = "Home | Precision and Fabrication Services";
 <template>
   <div>
     <Hero />
-    <div
-      class="
-        lg:w-1/2
-        mx-auto
-        text-2xl
-        my-20
-        lg:mb-20
-        mb-8
-        w-[90%]
-        text-center text-[#0E1E43]
-      "
-    >
-      We pride ourselves on providing a high quality services for precision
-      engineering and fabrication requirements.
-    </div>
-    <div class="lg:flex justify-evenly lg:w-4/5 mx-auto mb-20 text-[#0E1E43]">
-      <div class="p-8 lg:w-80 lg:m-0 m-6 border border-[#E9EBEC]">
-        <img class="w-12 h-12" src="../assets/check.png" alt="" />
-        <div class="font-bold my-2">Top Services</div>
-        <div>
-          As one of the leading precision engineering companies, we offer a
-          complete services from prototype to fabrication.
-        </div>
-      </div>
-      <div class="p-8 lg:w-80 lg:m-0 m-6 border border-[#E9EBEC]">
-        <img class="w-12 h-12" src="../assets/check.png" alt="" />
-        <div class="font-bold my-2">Industry Experience</div>
-        <div>
-          Our highly skilled team are experienced in working with a wide range
-          of materials and specifications.
-        </div>
-      </div>
-      <div class="p-8 lg:w-80 lg:m-0 m-6 border border-[#E9EBEC]">
-        <img class="w-12 h-12" src="../assets/check.png" alt="" />
-        <div class="font-bold my-2">Guaranteed Quality</div>
-        <div>
-          We are dedicated to delivering excellent quality and reliability
-          across our business and are accredited to ISO:9001.
-        </div>
-      </div>
-    </div>
-    <div class="lg:flex bg-[#0E1E43]">
-      <div
-        class="
-          lg:w-1/2 lg:p-32
-          px-6
-          py-16
-          lg:text-4xl
-          text-3xl
-          my-auto
-          bg-[#0E1E43]
-          text-white
-        "
-      >
-        We take a technological driven approach from prototyping to production
-        that further projects how we deliver to our clients.
-      </div>
-      <div>
-        <img src="../assets/side.png" alt="" />
-      </div>
-    </div>
+
     <div class="lg:flex lg:py-20 py-10">
       <div class="lg:w-[500px] lg:pl-20 pl-8 pr-8 lg:mt-32">
         <div class="text-[#0E1E43]">SERVICES AT CARMEL’S</div>
@@ -141,6 +81,68 @@ document.title = "Home | Precision and Fabrication Services";
               </div>
             </router-link>
           </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="lg:flex bg-[#0E1E43]">
+      <div
+        class="
+          lg:w-1/2 lg:p-32
+          px-6
+          py-16
+          lg:text-4xl
+          text-3xl
+          my-auto
+          bg-[#0E1E43]
+          text-white
+        "
+      >
+        We’re your CNC partner from prototyping to production on specifications.
+      </div>
+      <div>
+        <img src="../assets/side.png" alt="" />
+      </div>
+    </div>
+
+    <div
+      class="
+        lg:w-1/2
+        mx-auto
+        text-2xl
+        my-20
+        lg:mb-20
+        mb-8
+        w-[90%]
+        text-center text-[#0E1E43]
+      "
+    >
+      We pride ourselves on providing a high quality services for precision
+      engineering and fabrication requirements.
+    </div>
+    <div class="lg:flex justify-evenly lg:w-4/5 mx-auto mb-20 text-[#0E1E43]">
+      <div class="p-8 lg:w-80 lg:m-0 m-6 border border-[#E9EBEC]">
+        <img class="w-12 h-12" src="../assets/check.png" alt="" />
+        <div class="font-bold my-2">Top Services</div>
+        <div>
+          As one of the leading precision engineering companies, we offer a
+          complete services from prototype to fabrication.
+        </div>
+      </div>
+      <div class="p-8 lg:w-80 lg:m-0 m-6 border border-[#E9EBEC]">
+        <img class="w-12 h-12" src="../assets/check.png" alt="" />
+        <div class="font-bold my-2">Industry Experience</div>
+        <div>
+          Our highly skilled team are experienced in working with a wide range
+          of materials and specifications.
+        </div>
+      </div>
+      <div class="p-8 lg:w-80 lg:m-0 m-6 border border-[#E9EBEC]">
+        <img class="w-12 h-12" src="../assets/check.png" alt="" />
+        <div class="font-bold my-2">Guaranteed Quality</div>
+        <div>
+          We are dedicated to delivering excellent quality and reliability
+          across our business and are accredited to ISO:9001.
         </div>
       </div>
     </div>
